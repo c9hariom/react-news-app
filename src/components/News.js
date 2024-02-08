e6c5dbd54d56197574199560478184b4082f19ce
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-02-08 10:20:32 +0530

    pagination added

--- src/components/News.js
+++ src/components/News.js
@@ -8,18 +8,31 @@ export default class News extends Component {
     this.key='7ff22dbf91854124aaed0fd4670b0977';
     this.state = {
       articles: this.articles,
-      loading: false
+      loading: false,
+      pageNo:1,
+      totalPage:1,
+      pageSize:9
     }
   }
 
 
   async componentDidMount(){
-    console.log('cdm')
-    let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key;
+    //console.log('cdm')
+    let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page=1';
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData.articles);
-    this.setState({articles : parsedData.articles})
+    //console.log(parsedData.articles,typeof(parsedData.totalResults));
+    this.setState({articles : parsedData.articles});
+    this.setState({totalPage: Math.ceil(parsedData.totalResults/this.state.pageSize)});
+    console.log(parsedData,this.state.totalPage);
+  }
+
+  handleClick = async (para)=>{
+    let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page='+para;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    //console.log(parsedData.articles,typeof(parsedData.totalResults));
+    this.setState({articles : parsedData.articles});
   }
 
   render () {
@@ -44,7 +57,20 @@ export default class News extends Component {
             )
           }
         </div>
-      </div>
+        <div className="container">
+        <div className="d-flex justify-content-center">
+            <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(1)}>&#8592; Previous</button>
+            {
+              this.state.totalPage>1 && Array.from({length: this.state.totalPage},(v,i)=>{
+                return (
+                  <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(i+1)}>{i+1}</button>
+                )
+              })
+            }
+            <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(this.state.totalPage)}>Next &rarr;</button>
+</div>
+        </div>
+    </div>
     )
   }
 }
