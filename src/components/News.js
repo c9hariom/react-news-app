28e3e0eb91a6865c2372ab4bb0d56443bdbcbfe3
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-02-08 12:53:10 +0530

    pagination for news enhanced and finalized

--- src/components/News.js
+++ src/components/News.js
@@ -24,15 +24,31 @@ export default class News extends Component {
     //console.log(parsedData.articles,typeof(parsedData.totalResults));
     this.setState({articles : parsedData.articles});
     this.setState({totalPage: Math.ceil(parsedData.totalResults/this.state.pageSize)});
-    console.log(parsedData,this.state.totalPage);
+    //console.log(parsedData,this.state.totalPage);
   }
 
   handleClick = async (para)=>{
-    let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page='+para;
-    let data = await fetch(url);
+    let url='';
+    if(para === 'prev' && this.state.pageNo>1){
+      url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page='+(this.state.pageNo-1);
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({articles : parsedData.articles});
+      this.setState({pageNo: this.state.pageNo-1});
+    } else if (para === 'next' && this.state.pageNo < this.state.totalPage){
+      console.log('next'+this.state.pageNo)
+      url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page='+(this.state.pageNo+1);
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({articles : parsedData.articles});
+      this.setState({pageNo: this.state.pageNo+1});
+    } else if(para !=='prev' && para!=='next') {
+      this.setState({pageNo: para});
+      url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.key+'&pageSize='+this.state.pageSize+'&page='+para;
+      let data = await fetch(url);
     let parsedData = await data.json();
-    //console.log(parsedData.articles,typeof(parsedData.totalResults));
     this.setState({articles : parsedData.articles});
+    }
   }
 
   render () {
@@ -59,15 +75,15 @@ export default class News extends Component {
         </div>
         <div className="container">
         <div className="d-flex justify-content-center">
-            <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(1)}>&#8592; Previous</button>
+            <button className="btn my-2 btn btn-primary" type="button" disabled={this.state.pageNo===1?true:false} onClick={()=>this.handleClick('prev')}>&#8592; Previous</button>
             {
               this.state.totalPage>1 && Array.from({length: this.state.totalPage},(v,i)=>{
                 return (
-                  <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(i+1)}>{i+1}</button>
+                  <button disabled={this.state.pageNo===i+1?true:false} id={i+1} key={i+1} className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(i+1)}>{i+1}</button>
                 )
               })
             }
-            <button className="btn btn-primary my-2 mx-2" type="button" onClick={()=>this.handleClick(this.state.totalPage)}>Next &rarr;</button>
+            <button className="btn btn-primary my-2 mx-2" type="button" disabled={this.state.pageNo===this.state.totalPage?true:false} onClick={()=>this.handleClick('next')}>Next &rarr;</button>
 </div>
         </div>
     </div>
